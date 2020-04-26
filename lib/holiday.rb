a2f49ea2235894a642bcfc92ba468399382221f0
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-26 22:23:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,9 +16,9 @@ holiday_supplies = {
   }
 }  
   
-def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_supplies[:summer] [:fourth_of_july] [1]
-end
+# def second_supply_for_fourth_of_july(holiday_hash)
+#   holiday_supplies[:summer] [:fourth_of_july] [1]
+# end
 
 def add_supply_to_winter_holidays(holiday_hash, item)
   holiday_hash[winter].each do |holiday, decorations|
