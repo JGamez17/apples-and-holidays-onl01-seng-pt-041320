0696791392b7c19c1a22faff7c69c881759f3974
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-26 22:34:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,6 @@ def add_supply_to_winter_holidays(holiday_hash, item)
   holiday_hash[winter].each do |holiday, decorations|
     decorations << item
   end 
-end  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
 holiday_hash[:spring] [:memorial_day] << supply
