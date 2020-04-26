7db44c9fcd5d98f1c992774a18326d7c38020f3e
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-26 22:10:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,96 @@ end
 
 
 
+holiday_supplies = {
+  :winter => {
+    :Christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}  
+  
+def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_supplies[:summer] [:fourth_of_july] [1]
+end
+
+def add_supply_to_winter_holidays(holiday_hash, item)
+  holiday_hash[winter].each do |holiday, decorations|
+    decorations << item
+  end 
+end  
+
+def add_supply_to_memorial_day(holiday_hash, supply)
+holiday_hash[:spring] [:memorial_day] << supply
+end
+
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_name[season] [holiday_name] = supply_array
+  holiday_hash
+end
+
+def all_winter_holiday_supplies(holiday_hash)
+holiday_hash[:winter].values.flatten  
+end  
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}"
+    holiday.each do |holiday, item|
+      puts " #{holiday.to_s.split("_").collect {|1| 1.capitalize}.join(" ") }: #{item.join (", ")}"   
+      holiday.each do |holiday, item|
+        puts " #{holiday.to_s.split("_").collect {|1| 1.capitalize.join(" ")}: #item.join(", ")}"
+      end  
+    end
+  end 
+  
+  
+  
+  
+  
+
+
+
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.collect do |season, holiday|
+    holiday.collect do |holiday, item|
+      holiday if item.include?("BBQ")
+    end
+  end.flatten.compact
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do | season, holiday|
+    puts "#{season.capitalize}"
+
 
 
 
